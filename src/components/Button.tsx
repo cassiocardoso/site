ac17c01d4b77ae02f3a9de714f43ab7dce06d029
Cassio Cardoso <caugusto.cardoso@gmail.com>
2020-04-16 11:49:12 +0200

    change primary button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ import theme from '../../config/Theme';
 
 export const Button: any = styled.button`
   align-items: center;
-  background: ${props => props.theme.colors.primary};
+  background: ${props => props.theme.colors.ceruleanBlue};
   border-radius: ${(props: any) => (props.big ? '1.5rem' : '1rem')};
   border: none;
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
@@ -17,7 +17,7 @@ export const Button: any = styled.button`
   transition: all ${(props: any) => props.theme.transitions.normal};
 
   &:hover {
-    background: ${(props: any) => curriedDarken(0.3, props.theme.colors.primary)};
+    background: ${(props: any) => curriedDarken(0.2, props.theme.colors.ceruleanBlue)};
     cursor: pointer;
     transform: translateY(-0.1rem);
   }
