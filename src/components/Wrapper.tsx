cdd3487c60982a06b11253000346f156e94ca36a
Cassio Cardoso <caugusto.cardoso@gmail.com>
2020-05-21 15:00:53 +0200

    reduce wrapper max-width

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -5,7 +5,7 @@ export const Wrapper: any = styled.div`
   display: flex;
   flex-direction: column;
   margin: 0 auto;
-  max-width: ${(props: any) => (props.fullWidth ? '100%' : '100rem')};
+  max-width: ${(props: any) => (props.fullWidth ? '100%' : '80rem')};
   padding: ${(props: any) => (props.fullWidth ? '0' : '0 1rem')};
 
   @media ${mediaV2.tablet} {
