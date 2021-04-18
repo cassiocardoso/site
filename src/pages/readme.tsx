9f59218bef6132744325198ef6790cfcf4e80ff1
Cassio Cardoso <cassio.cardoso@vimcar.com>
2021-04-18 10:02:12 +0200

    feat(readme): add readme page

---
+++ src/pages/readme.tsx
@@ -0,0 +1,128 @@
+import { FC } from 'react';
+
+import { PageLayout } from 'layouts/Page';
+
+const UsesPage: FC = () => (
+  <PageLayout title="Readme">
+    <h2>What is this?</h2>
+    <p>
+      This is a collection of thoughts on how I behave as a manager and what I expect from you as an
+      employee. This is a living document, which means that some opinions here may change over time.
+      But if you see me behaving differently from what is described here, let me know asap.
+    </p>
+    <h2>
+      What this is <strong>NOT</strong>
+    </h2>
+    <p>
+      This is not a replacement for we getting to know each other during the daily routines and
+      one-on-ones. Hopefully, this is a helpful resource in the beginning.
+    </p>
+    <h2>Communication</h2>
+    <p>
+      I&apos;m fairly open to talk about my hobbies, work, career, and personal matters. If you want
+      to know something, ask me. I hope that over time we can build a trusting relationship where we
+      can talk openly about all of this.
+    </p>
+    <p>
+      I expect you to talk about your problems. If something happened let me know as soon as
+      possible and we will find some time to talk about it.
+    </p>
+    <p>
+      If something urgent comes up, don&apos;t wait for the next one-on-one, write me on Slack and
+      let&apos;s talk.
+    </p>
+    <p>
+      I like to keep my agenda public, you are encouraged to find a free slot and schedule a meeting
+      there any time you need.
+    </p>
+    <p>
+      I expect that you contribute with ideas, raise problems and propose solutions, and make that
+      communication as effectively as possible.
+      <strong>Good Communication + Collaboration {'>'} Individual Tech Expertise</strong>
+    </p>
+    <p>
+      I respect the ability to respond immediately and the need to thoughtfully reflect and get back
+      to me. I&apos;m the latter, so you can expect that from me.
+    </p>
+    <h2>The Actual work</h2>
+    <p>Own your work. Ask questions, clarify, test, cover edge cases, deploy, monitor.</p>
+    <p>
+      Own the failures, treat them as learning opportunities. Be transparent, ask for help if you
+      can&apos;t find the root cause. Share the learning. I&apos;ve got you back 100% of the time.
+    </p>
+    <p>
+      Show Up. Don&apos;t be late to meetings. If you can&apos;t avoid it, let the others know asap.
+    </p>
+    <p>
+      I expect you to become a better coder than I am (if you are not already), that means I will
+      trust you to make the best decisions. I will, however, ask clarifying questions so I can fully
+      understand it.
+    </p>
+    <p>
+      You can use me as a rubber duck at any time. In fact, I will be happy to hear your ideas and
+      provide some context if you need.
+    </p>
+    <h2>Transparency</h2>
+    <p>
+      The company may ask me to hold information for some time before announcing. They can&apos;t
+      ask me to lie to you. I would never agree to do that.
+    </p>
+    <p>
+      I bias towards transparency and candor. If you want to know something, ask. I will try my best
+      to always answer to any question. If I can&apos;t or don&apos;t know the answer, I&apos;ll do
+      my best to figure it out and come back to you.
+    </p>
+    <h2>Feedback Loop</h2>
+    <p>
+      I&apos;ll always give you direct feedback as quickly as possible. Sometimes with action
+      points, other times I&apos;ll let it to you to come up with them.
+    </p>
+    <p>
+      I expect you to do the same for me. I&apos;m here to help you, but I need to know where
+      I&apos;m failing to improve.
+    </p>
+    <p>
+      Personal and organizational goals should be aligned and heading towards the same direction. If
+      this is not happening, let me know and we&apos;ll figure it out.
+    </p>
+    <h2>Professional Development</h2>
+    <p>
+      Own your career path. I&apos;m here to help, guide and provide opportunities for you to
+      achieve what you want.
+    </p>
+    <p>
+      If you are unsure about what to do, give each option a try. This is the best way to know how
+      it would play out. I expect you to redefine your goals from time to time and to communicate
+      those with me so we can align our plans and look for new opportunities.
+    </p>
+    <p>
+      Keep pushing yourself. Be eager to learn, ask for opportunities. Continue to develop your
+      skills.
+    </p>
+    <h2>One on ones</h2>
+    <p>
+      These meetings are for <strong>us</strong> (but mostly, for you). I expect you to bring
+      what&apos;s on your mind, successes, concerns, and above all, how I can help you.
+    </p>
+    <p>
+      It shouldn&apos;t be focused on project updates, this can be done at different times. We can
+      discuss tech stuff but we should focus on what you need to do to overcome the roadblocks.
+    </p>
+    <p>
+      I&apos;ll keep track of our meetings and I expect you to drive the direction of the meetings.
+      If you don&apos;t have any topics, I&apos;ll always have a few questions to ask.
+    </p>
+    <p>
+      The focus is to build a relationship. I&apos;m here to support you and your growth. Your part
+      is letting me know how.
+    </p>
+    <p>
+      <strong>
+        Urgent matters shouldn&apos;t wait for a one-on-one. Slack or call me and we&apos;ll talk
+        asap.
+      </strong>
+    </p>
+  </PageLayout>
+);
+
+export default UsesPage;
